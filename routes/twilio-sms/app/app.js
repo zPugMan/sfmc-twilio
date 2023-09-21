517279f230f29e2073569ddfc1fa76d254235971
Zachary Bethem <zbethem@outlook.com>
2023-09-21 14:56:52 -0700

    initial structure for custom activity via express module

---
+++ routes/twilio-sms/app/app.js
@@ -0,0 +1,39 @@
+// Journey Builder Custom Activity -- twilio-sms Activity
+// ````````````````````````````````````````````````````````````
+// SERVER SIDE IMPLEMENTATION
+//
+// This example demonstrates
+// * Configuration Lifecycle Events
+//    - save
+//    - publish
+//    - validate
+// * Execution Lifecycle Events
+//    - execute
+//    - stop
+
+const express = require('express')
+const configJSON = require('../config/config-json')
+
+module.exports = function twilioSmsActivity(app, options) {
+    const activityDir = `${options.rootDirectory}/routes/twilio-sms`;
+
+    //static resources
+    app.use('/routes/twilio-sms/dist', express.static(`${activityDir}/dist`));
+    app.use('/routes/twilio-sms/images', express.static(`${activityDir}/images`));
+
+    // index redirect
+    app.get('/routes/twilio-sms', function(req, res) {
+        console.log(`Redirection: ${activityDir}/index.html`);
+        return res.redirect(`/routes/twilio-sms/index.html`);
+    });
+
+    // index.html route
+    app.get('/routes/twilio-sms/index.html', function(req,res) {
+        return res.sendFile(`${activityDir}/html/index.html`);
+    });
+
+    // config.json ruote
+    app.get('/routes/twilio-sms/config.json', function(req,res){
+        return res.status(200).json(configJSON(req));
+    });
+};
---
+++ routes/twilio-sms/config/config-json.js
@@ -0,0 +1,82 @@
+module.exports = function configJSON(req) {
+    return {
+        workflowApiVersion: '1.1',
+        metaData: {
+          // the location of our icon file
+          icon: `images/twilio-tile.svg`,
+          category: 'message'
+        },
+        // For Custom Activity this must say, "REST"
+        type: 'REST',
+        lang: {
+          'en-US': {
+            name: 'Twilio SMS',
+            description: 'Sends SMS via Twilio'
+          }
+        },
+        arguments: {
+          execute: {
+            // See: https://developer.salesforce.com/docs/atlas.en-us.mc-apis.meta/mc-apis/how-data-binding-works.htm
+            inArguments: [
+              {
+                contactKey: ""
+              }
+            ],
+            outArguments: [],
+            // Fill in the host with the host that this is running on.
+            // It must run under HTTPS
+            url: `https://${req.headers.host}/twilio-sms/execute`,
+            // The amount of time we want Journey Builder to wait before cancel the request. Default is 60000, Minimal is 1000
+            timeout: 10000,
+            // how many retrys if the request failed with 5xx error or network error. default is 0
+            retryCount: 3,
+            // wait in ms between retry.
+            retryDelay: 1000,
+            // The number of concurrent requests Journey Builder will send all together
+            concurrentRequests: 5
+          }
+        },
+        configurationArguments: {
+          publish: {
+            url: `https://${req.headers.host}/twilio-sms/publish`
+          },
+          validate: {
+            url: `https://${req.headers.host}/twilio-sms/validate`
+          },
+          stop: {
+            url: `https://${req.headers.host}/twilio-sms/stop`
+          }
+        },
+        userInterfaces: {
+          configurationSupportsReadOnlyMode : true,
+          configInspector: {
+            size: 'scm-lg',
+            emptyIframe: true
+          }
+        },
+        schema: {
+          arguments: {
+            execute: {
+              inArguments: [],
+              outArguments: [{
+                status: {
+                  dataType: 'Text',
+                  direction: 'out',
+                  access: 'visible'
+                },
+                errorCode: {
+                  dataType: 'Number',
+                  direction: 'out',
+                  access: 'visible'
+                },
+                errorMessage: {
+                    dataType: 'Text',
+                    direction: 'out',
+                    access: 'visible'
+                }
+              }]
+            }
+          }
+        }
+      };
+    };
